5f45e2c27e69d82632f21e0794b83edf0e6cfdd1
SebastianAlba84 <sebasjosue84@gmail.com>
2018-05-14 05:47:03 -0600

    Se prueba Chunk

--- Cliente/Sockets.cs
+++ Cliente/Sockets.cs
@@ -7,14 +7,40 @@ namespace Cliente
 {
     class Sockets
     {
+<<<<<<< HEAD
         public static string Conectar(int work, string nick, string name, string edad, string genero, string contra, string amigos)
         {
             TcpClient client = new TcpClient("localhost", 113);
+=======
+        public static string Conectar(int work, string String1, string String2, string String3, string String4, string String5, string String6)
+        {
+            TcpClient client = new TcpClient("localhost", 8000);
+            if (work == 0)
+            {
+                string data = work.ToString();
+                byte[] buf;
+                buf = Encoding.UTF8.GetBytes(data + "\n");
+                NetworkStream stream = client.GetStream();
+                stream.Write(buf, 0, data.Length + 1);
+                buf = new byte[100];
+                stream.Read(buf, 0, 100);
+                string xml = Encoding.UTF8.GetString(buf);
+                xml = xml.Substring(0, xml.IndexOf(char.ConvertFromUtf32(0)));
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                string nombre = doc.DocumentElement.InnerText;
+                return nombre;
+            }
+>>>>>>> ce26871e79a0e822fa38d7c850e0ab12fd67706b
 
             if (work == 1)
             {
 
+<<<<<<< HEAD
                 string data = work.ToString() + "/" + nick + "/" + name + "/" + edad + "/" + genero + "/" + contra + "/" + amigos;
+=======
+                string data = work.ToString() + "/" + String1 + "/" + String2 + "/" + String3 + "/" + String4 + "/" + String5 + "/" + String6;
+>>>>>>> ce26871e79a0e822fa38d7c850e0ab12fd67706b
                 byte[] buf;
                 buf = Encoding.UTF8.GetBytes(data + "\n");
                 NetworkStream stream = client.GetStream();
@@ -35,6 +61,58 @@ namespace Cliente
                 {
                     return "false";
                 }
+<<<<<<< HEAD
+=======
+            }
+            if (work == 2)
+            {
+                string data = work.ToString() + "/" +  String1 + "/" + String2;
+                byte[] buf;
+                buf = Encoding.UTF8.GetBytes(data + "\n");
+                NetworkStream stream = client.GetStream();
+                stream.Write(buf, 0, data.Length + 1);
+                buf = new byte[100];
+                stream.Read(buf, 0, 100);
+                string xml = Encoding.UTF8.GetString(buf);
+                xml = xml.Substring(0, xml.IndexOf(char.ConvertFromUtf32(0)));
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                return doc.DocumentElement.Name;
+            }
+            if(work == 7)
+            {
+                string data = work.ToString() + "/" + String1 + "/" + String2 + "/" + String3;
+                byte[] buf;
+                buf = Encoding.UTF8.GetBytes(data + "\n");
+                NetworkStream stream = client.GetStream();
+                stream.Write(buf, 0, data.Length + 1);
+                buf = new byte[100];
+                stream.Read(buf, 0, 100);
+                string xml = Encoding.UTF8.GetString(buf);
+                xml = xml.Substring(0, xml.IndexOf(char.ConvertFromUtf32(0)));
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                return doc.DocumentElement.Name;
+            }
+            if (work == 8)
+            {
+
+            }
+            if (work == 9)
+            {
+                string data = work.ToString() + "/" + String1 ;
+                byte[] buf;
+                buf = Encoding.UTF8.GetBytes(data + "\n");
+                NetworkStream stream = client.GetStream();
+                stream.Write(buf, 0, data.Length + 1);
+                buf = new byte[100];
+                stream.Read(buf, 0, 100);
+                string xml = Encoding.UTF8.GetString(buf);
+                xml = xml.Substring(0, xml.IndexOf(char.ConvertFromUtf32(0)));
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xml);
+                return doc.DocumentElement.Name;
+>>>>>>> ce26871e79a0e822fa38d7c850e0ab12fd67706b
             }
             return "false";
         }
--- Cliente/Usuario.cs
+++ Cliente/Usuario.cs
@@ -19,11 +19,15 @@ namespace Cliente
 
         private void button1_Click(object sender, EventArgs e)
         {
+<<<<<<< HEAD
             int i = 0;
+=======
+>>>>>>> ce26871e79a0e822fa38d7c850e0ab12fd67706b
             if (boxNick.TextLength > 0)
             {
                 if (boxName.TextLength > 0)
                 {
+<<<<<<< HEAD
                     if (boxEdad.TextLength > 0) // validar que es numero
                     {
                         if (boxContra.TextLength > 0)
@@ -55,6 +59,50 @@ namespace Cliente
                         else
                         {
                             MessageBox.Show("Se debe escribir una Contraseña");
+=======
+                    if (boxEdad.TextLength > 0)
+                    {
+                        try
+                        {
+                            Int32.Parse(boxEdad.Text);
+                            if (boxContra.TextLength > 0)
+                            {
+                                string genero = "";
+                                foreach (string s in boxGenero.CheckedItems)
+                                {
+                                    genero = genero + s + ",";
+                                }
+                                string nick = boxNick.Text;
+                                string name = boxName.Text;
+                                string edad = boxEdad.Text;
+                                string contra = boxContra.Text;
+                                string amigos = "";
+                                foreach (string s in box.CheckedItems)
+                                {
+                                    amigos = amigos + s + ",";
+                                }
+                                string resultado = "true";//Sockets.Conectar(1, nick, name, edad, genero, contra, amigos);
+
+                                if (resultado == "true")
+                                {
+                                    MessageBox.Show("Se ha creado el nuevo usuario");
+                                    Login login = new Login();
+                                    login.Show();
+                                    this.Close();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("El Apodo ya esta en uso");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Se debe escribir una Contraseña");
+                            }
+                        }
+                        catch {
+                            MessageBox.Show("La edad ingresada debe ser un numero");
+>>>>>>> ce26871e79a0e822fa38d7c850e0ab12fd67706b
                         }
                     }
                     else
@@ -80,7 +128,30 @@ namespace Cliente
 
         private void Usuario_Load(object sender, EventArgs e)
         {
-
+            string nombre = Sockets.Conectar(0, "", "", "", "", "", "");
+            string[] gen = new string[10];
+            int z = 0;
+            int j = 0;
+            int n = 1;
+            for (int c = 0; nombre.Length > c; c++)
+            {
+                if (nombre.Substring(c, 1) == "/")
+                {
+                    gen[z] = nombre.Substring(j, n - 1);
+                    Console.WriteLine(gen[z]);
+                    z++;
+                    j = c + 1;
+                    n = 0;
+                }
+                n++;
+            }
+            foreach (string usuario in gen)
+            {
+                if (usuario != null)
+                {
+                    box.Items.Add(usuario);
+                }
+            }
         }
     }
 }
